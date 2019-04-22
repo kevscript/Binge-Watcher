61fa50b132a78014e367327b5753b8eb08179549
Kevin Ostafinski <kevinostafinski@gmail.com>
2019-04-22 18:59:52 +0200

    added menu toggling + added primary color to burger

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Route, Switch, withRouter, Redirect } from 'react-router-dom'
 import styled from 'styled-components'
 import MoviesPage from './MoviesPage'
@@ -6,24 +6,28 @@ import MoviePage from './MoviePage'
 import ProfilePage from './ProfilePage'
 import SearchPage from './SearchPage'
 import Sidebar from '../components/Sidebar'
+import Burger from '../components/Burger'
 
 const Container = styled.div`
   display: flex;
 `
 
 const SidebarContainer = styled.div`
+  padding-top: 50px;
   padding-left: 50px;
   height: 100vh;
   overflow-y: scroll;
   width: 300px;
   overflow-x: hidden;
   position: fixed;
-  left 0;
+  left: 0;
   transition: all 0.5s;
 
   @media (max-width: 1000px) {
-    left: -300px;
+    z-index: 98;
+    left: ${props => props.toggled ? 0 : '-300px'};
     transition: all 0.5s;
+    background: #fff;
   }
 `
 
@@ -35,16 +39,55 @@ const MainContainer = styled.div`
   @media (max-width: 1000px) {
     width: 100%;
     margin-left: 0;
-    transition: all 0.5s;
+    margin-top: 50px;
+  }
+`
+
+const Menu = styled.div`
+  cursor: pointer;
+  z-index: 99;
+  position: fixed;
+  top: 15px;
+  left: -50px;
+
+  @media (max-width: 1000px) {
+    left: ${props => props.toggled ? '225px' : '20px'};
+    transition: all 0.3s;
   }
 `
 
 const App = () => {
+
+  const [toggleMenu, setToggleMenu] = useState(false)
+  const [windowWidth, setWindowWidth] = useState(0)
+
+  useEffect(() => {
+    window.addEventListener('resize', handleWindow)
+  })
+
+  const handleMenu = () => {
+    setToggleMenu(!toggleMenu)
+  }
+
+  const handleWindow = () => {
+    setWindowWidth(window.innerWidth)
+
+    if (window.innerWidth >= 1000) {
+      setToggleMenu(false)
+    }
+  }
+
+
   return (
     <Container>
-      <SidebarContainer>
+      <Menu onClick={handleMenu} toggled={toggleMenu}>
+        <Burger />
+      </Menu>
+
+      <SidebarContainer toggled={toggleMenu}>
         <Sidebar />
       </SidebarContainer>
+
       <MainContainer>
         <Switch>
           <Route exact path='/' render={() => (
