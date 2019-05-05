9d49d87a2cf5358b6132b2f18326a3143df27393
Kevin Ostafinski <kevinostafinski@gmail.com>
2019-05-05 23:38:17 +0200

    added Error component to SearchPage component and styled the query section

--- src/containers/SearchPage.js
+++ src/containers/SearchPage.js
@@ -2,16 +2,30 @@ import React, { useEffect } from 'react'
 import { connect } from 'react-redux'
 import { fetchSearch } from '../actions'
 import Spinner from '../components/Spinner'
+import Error from '../components/Error'
 import MoviesList from '../components/MoviesList'
 import Pagination from '../components/Pagination'
 import styled from 'styled-components'
 
 const SearchPageContainer = styled.div`
   width: 90%;
-  margin: 0 auto
+  margin: 0 auto;
 `
 
+const TitleContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
 
+const SearchSubtitle = styled.h3`
+  font-size: 16px;
+`
+
+const SearchQuery = styled.span`
+  font-size: 16px;
+  color: ${props => props.theme.colors.primary};
+`
 
 const SearchPage = ({ fetchSearch, search, match }) => {
 
@@ -23,11 +37,17 @@ const SearchPage = ({ fetchSearch, search, match }) => {
 
   if (loading) {
     return <Spinner size={100} loading={loading} />
+  } else if (results === undefined || results.length === 0) {
+    return (
+      <Error message='No Results Found.' />
+    )
   } else {
     return (
       <SearchPageContainer>
-        <p>Search results for {match.params.value}</p>
         <Pagination fetchData={fetchSearch} page={page} totalPages={total_pages} query={match.params.value} />
+        <TitleContainer>
+          <SearchSubtitle>Results for '<SearchQuery>{match.params.value}</SearchQuery>'</SearchSubtitle>
+        </TitleContainer>
         <MoviesList data={results} />
         <Pagination fetchData={fetchSearch} page={page} totalPages={total_pages} query={match.params.value} />
       </SearchPageContainer>
